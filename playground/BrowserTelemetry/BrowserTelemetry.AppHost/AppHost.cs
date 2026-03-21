148e19fbbfbef7b50b37a3872b0228fc96e0afa5
afscrome <afscrome@users.noreply.github.com>
2026-03-21 13:06:24 +0000

    Refactor SQL Server setup and connection handling with TLS support
    - Updated SQL Server resource to configure a TLS cert when one is configured, and remove the TLS validation bypass from the connection string
    - Updated Playground app host to include sql with no TLS, and renamed resources to make their purpose clearer

--- playground/BrowserTelemetry/BrowserTelemetry.AppHost/AppHost.cs
+++ playground/BrowserTelemetry/BrowserTelemetry.AppHost/AppHost.cs
@@ -1,6 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+<<<<<<<< HEAD:playground/BrowserTelemetry/BrowserTelemetry.AppHost/AppHost.cs
 #pragma warning disable ASPIREBROWSERLOGS001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
 
 var builder = DistributedApplication.CreateBuilder(args);
@@ -8,6 +9,32 @@ var builder = DistributedApplication.CreateBuilder(args);
 builder.AddProject<Projects.BrowserTelemetry_Web>("web")
     .WithExternalHttpEndpoints()
     .WithBrowserLogs();
+========
+#pragma warning disable ASPIRECERTIFICATES001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+
+var builder = DistributedApplication.CreateBuilder(args);
+
+var sqlLocal = builder.AddAzureSqlServer("sqlLocal")
+    .RunAsContainer();
+
+var dbLocal = sqlLocal.AddDatabase("dbLocal");
+
+var sqlAzure = builder.AddAzureSqlServer("sqlAzure");
+var dbAzure = sqlAzure.AddDatabase("dbAzure");
+
+var sqlNoTls = builder.AddSqlServer("sqlNoTls")
+    .WithoutHttpsCertificate();
+
+var dbsetup = builder.AddProject<Projects.SqlServerEndToEnd_DbSetup>("dbsetup")
+                     .WithReference(dbLocal).WaitFor(sqlLocal)
+                     .WithReference(dbAzure).WaitFor(sqlAzure);
+
+builder.AddProject<Projects.SqlServerEndToEnd_ApiService>("api")
+       .WithExternalHttpEndpoints()
+       .WithReference(dbLocal).WaitFor(dbLocal)
+       .WithReference(dbAzure).WaitFor(dbAzure)
+       .WaitForCompletion(dbsetup);
+>>>>>>>> 8e68a8d71 (Refactor SQL Server setup and connection handling with TLS support):playground/SqlServerEndToEnd/SqlServerEndToEnd.AppHost/AppHost.cs
 
 #if !SKIP_DASHBOARD_REFERENCE
 // This project is only added in playground projects to support development/debugging
