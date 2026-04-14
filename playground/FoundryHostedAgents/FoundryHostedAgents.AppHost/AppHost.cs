a92606bc5a731957f9516c7edbcbcc7ac0758707
Tommaso Stocchi <tommasostocchi@outlook.com>
2026-04-14 17:15:08 +0200

    fixes on the playground

--- playground/FoundryHostedAgents/FoundryHostedAgents.AppHost/AppHost.cs
+++ playground/FoundryHostedAgents/FoundryHostedAgents.AppHost/AppHost.cs
@@ -5,22 +5,19 @@ using Aspire.Hosting.Foundry;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-var tenantId = builder.AddParameterFromConfiguration("tenant", "Azure:TenantId");
-
 var foundry = builder.AddFoundry("aif-myfoundry");
 var project = foundry.AddProject("proj-myproject");
 var chat = project.AddModelDeployment("chat", FoundryModel.OpenAI.Gpt41);
 
 builder.AddPythonApp("weather-hosted-agent", "../app", "main.py")
     .WithUv()
-    .WithReference(project).WaitFor(project)
     .WithReference(chat).WaitFor(chat)
     .WithEnvironment("AZURE_TENANT_ID", tenantId)
     .PublishAsHostedAgent(project);
 
 builder.AddProject<Projects.DotNetHostedAgent>("proj-dotnet-hosted-agent")
     .WithReference(chat).WaitFor(chat)
-    .PublishAsHostedAgent()
+    .PublishAsHostedAgent(project)
     .WithEndpoint("http", e => e.TargetPort = 9000);
 
 builder.Build().Run();
