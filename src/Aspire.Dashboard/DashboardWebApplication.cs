a357150d77bac89067d5ad548b0c898b0be4d5ae
James Newton-King <james@newtonking.com>
2026-05-16 12:28:18 +0800

    Change dashboard version log message

--- src/Aspire.Dashboard/DashboardWebApplication.cs
+++ src/Aspire.Dashboard/DashboardWebApplication.cs
@@ -555,7 +555,7 @@ public sealed class DashboardWebApplication : IAsyncDisposable
         {
             // Write version at info level so it's written to the console by default. Help us debug user issues.
             // Display version and commit like 8.0.0-preview.2.23619.3+17dd83f67c6822954ec9a918ef2d048a78ad4697
-            logger.LogInformation("Aspire version: {Version}", informationalVersion);
+            logger.LogInformation("Aspire dashboard version: {Version}", informationalVersion);
         }
     }
 
