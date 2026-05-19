31d58efb63e79ce429322f9dbdd31c4c52a42207
James Newton-King <james@newtonking.com>
2026-05-20 00:43:02 +0800

    Fix

--- src/Aspire.Dashboard/Components/Dialogs/NotificationEntryComponent.razor.cs
+++ src/Aspire.Dashboard/Components/Dialogs/NotificationEntryComponent.razor.cs
@@ -16,6 +16,9 @@ public partial class NotificationEntryComponent : ComponentBase
     [Parameter]
     public EventCallback OnDismiss { get; set; }
 
+    [CascadingParameter]
+    public FluentDialog Dialog { get; set; } = default!;
+
     private string IntentClass => Entry.Intent switch
     {
         MessageIntent.Success => "intent-success",
