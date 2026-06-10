c1b969a01473dc4915bf116dc460beb9a9ae8e82
James Newton-King <james@newtonking.com>
2026-06-10 08:59:08 +0800

    Fix build

--- playground/Stress/Stress.AppHost/CommandResources.cs
+++ playground/Stress/Stress.AppHost/CommandResources.cs
@@ -876,7 +876,7 @@ internal static class CommandResources
                 displayName: "Stop all resources",
                 executeCommand: async (c) =>
                 {
-                    await ExecuteCommandForAllResourcesAsync(c.ServiceProvider, KnownResourceCommands.StopCommand, c.CancellationToken);
+                    await ExecuteCommandForAllResourcesAsync(c.Services, KnownResourceCommands.StopCommand, c.CancellationToken);
                     return CommandResults.Success();
                 },
                 commandOptions: new() { IconName = "Stop", IconVariant = IconVariant.Filled })
@@ -885,7 +885,7 @@ internal static class CommandResources
                 displayName: "Start all resources",
                 executeCommand: async (c) =>
                 {
-                    await ExecuteCommandForAllResourcesAsync(c.ServiceProvider, KnownResourceCommands.StartCommand, c.CancellationToken);
+                    await ExecuteCommandForAllResourcesAsync(c.Services, KnownResourceCommands.StartCommand, c.CancellationToken);
                     return CommandResults.Success();
                 },
                 commandOptions: new() { IconName = "Play", IconVariant = IconVariant.Filled });
