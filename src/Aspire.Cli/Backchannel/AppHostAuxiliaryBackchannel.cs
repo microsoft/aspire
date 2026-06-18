16d84bd4cc2f4acb4f00286889dc54ce5d7dcd95
James Newton-King <james@newtonking.com>
2026-06-18 21:46:54 +0800

    Fix XML doc param tag order to match actual parameter order

--- src/Aspire.Cli/Backchannel/AppHostAuxiliaryBackchannel.cs
+++ src/Aspire.Cli/Backchannel/AppHostAuxiliaryBackchannel.cs
@@ -123,9 +123,9 @@ internal sealed class AppHostAuxiliaryBackchannel : IAppHostAuxiliaryBackchannel
     /// Creates and connects a new auxiliary backchannel to the specified socket path.
     /// </summary>
     /// <param name="socketPath">The path to the Unix domain socket.</param>
-    /// <param name="logger">Optional logger for diagnostic messages.</param>
+    /// <param name="logger">Logger for diagnostic messages.</param>
+    /// <param name="profilingTelemetry">Profiling service.</param>
     /// <param name="cancellationToken">Cancellation token.</param>
-    /// <param name="profilingTelemetry">Optional profiling service.</param>
     /// <returns>A connected AppHostAuxiliaryBackchannel instance.</returns>
     public static Task<AppHostAuxiliaryBackchannel> ConnectAsync(
         string socketPath,
@@ -147,10 +147,10 @@ internal sealed class AppHostAuxiliaryBackchannel : IAppHostAuxiliaryBackchannel
     /// <param name="hash">The AppHost hash identifier.</param>
     /// <param name="socketPath">The socket path.</param>
     /// <param name="isInScope">Whether this AppHost is within the scope of the working directory.</param>
-    /// <param name="socket">Optional already-connected socket. If null, a new connection will be established.</param>
     /// <param name="logger">Logger.</param>
-    /// <param name="cancellationToken">Cancellation token (only used when socket is null).</param>
     /// <param name="profilingTelemetry">Profiling service.</param>
+    /// <param name="socket">Optional already-connected socket. If null, a new connection will be established.</param>
+    /// <param name="cancellationToken">Cancellation token (only used when socket is null).</param>
     /// <returns>A connected AppHostAuxiliaryBackchannel instance.</returns>
     internal static async Task<AppHostAuxiliaryBackchannel> CreateFromSocketAsync(
         string hash,
