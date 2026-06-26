c69e4a24e68420639d5dc05b14a39a67d23aa17e
James Newton-King <james@newtonking.com>
2026-06-26 22:16:05 +0800

    Remove Experimental attribute from CommandProgressOptions, suppress warning in playground

--- src/Aspire.Hosting/ApplicationModel/CommandOptions.cs
+++ src/Aspire.Hosting/ApplicationModel/CommandOptions.cs
@@ -1,8 +1,6 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Diagnostics.CodeAnalysis;
-
 namespace Aspire.Hosting.ApplicationModel;
 
 #pragma warning disable ASPIREINTERACTION001 // CommandProgressOptions is experimental.
@@ -117,7 +115,6 @@ public class CommandOptions
 /// <summary>
 /// Options for displaying a progress dialog while a command is executing.
 /// </summary>
-[Experimental("ASPIREINTERACTION001", UrlFormat = "https://aka.ms/aspire/diagnostics/{0}")]
 [AspireDto]
 public class CommandProgressOptions
 {
