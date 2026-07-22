6921bd883e7ff34cadcb06688d5ebcab5c045304
James Newton-King <james@newtonking.com>
2026-07-23 07:51:10 +0800

    Refine dashboard metric rollup intervals

--- src/Aspire.Dashboard/Components/Controls/Chart/MetricDataPointInterval.cs
+++ src/Aspire.Dashboard/Components/Controls/Chart/MetricDataPointInterval.cs
@@ -13,13 +13,41 @@ internal static class MetricDataPointInterval
     /// </summary>
     public static TimeSpan Get(TimeSpan duration)
     {
-        if (duration >= TimeSpan.FromHours(3))
+        if (duration <= TimeSpan.FromMinutes(5))
         {
-            return TimeSpan.FromMinutes(5);
+            return TimeSpan.FromSeconds(1);
         }
 
-        return duration <= TimeSpan.FromMinutes(15)
-            ? TimeSpan.FromSeconds(1)
-            : TimeSpan.FromMinutes(1);
+        if (duration <= TimeSpan.FromMinutes(15))
+        {
+            return TimeSpan.FromSeconds(2);
+        }
+
+        if (duration <= TimeSpan.FromMinutes(30))
+        {
+            return TimeSpan.FromSeconds(5);
+        }
+
+        if (duration <= TimeSpan.FromHours(1))
+        {
+            return TimeSpan.FromSeconds(10);
+        }
+
+        if (duration <= TimeSpan.FromHours(3))
+        {
+            return TimeSpan.FromSeconds(30);
+        }
+
+        if (duration <= TimeSpan.FromHours(6))
+        {
+            return TimeSpan.FromMinutes(1);
+        }
+
+        if (duration <= TimeSpan.FromHours(12))
+        {
+            return TimeSpan.FromMinutes(2);
+        }
+
+        return TimeSpan.FromMinutes(5);
     }
 }
