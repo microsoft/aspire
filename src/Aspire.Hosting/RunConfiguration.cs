9d01a942e95a872914616f843768ada06cf083dd
Karol Zadora-Przylecki <karolz@microsoft.com>
2026-07-28 09:53:03 -0700

    Make RunConfiguration doc comments more concise

--- src/Aspire.Hosting/RunConfiguration.cs
+++ src/Aspire.Hosting/RunConfiguration.cs
@@ -6,28 +6,15 @@ using System.Diagnostics.CodeAnalysis;
 namespace Aspire.Hosting;
 
 /// <summary>
-/// Describes how the AppHost is being run when <see cref="DistributedApplicationExecutionContext.Operation"/>
-/// is <see cref="DistributedApplicationOperation.Run"/>.
+/// Holds settings applicable to the AppHost run mode (when <see cref="DistributedApplicationExecutionContext.Operation"/>
+/// is <see cref="DistributedApplicationOperation.Run"/>).
 /// </summary>
 /// <remarks>
-/// <para>
-/// Each property describes an independent aspect of the run, so additional run behaviors can be introduced
-/// over time without collapsing them into a single mutually exclusive mode.
-/// </para>
 /// <para>
 /// Integrations use it to vary how their resources are launched without changing the core hosting behavior.
 /// In <see cref="DistributedApplicationOperation.Publish"/> mode every property holds its default value.
 /// </para>
 /// </remarks>
-/// <example>
-/// This example launches a resource differently when the AppHost is running in watch mode:
-/// <code>
-/// if (builder.ExecutionContext.RunConfiguration.WatchEnabled)
-/// {
-///     // Launch the resource so that source changes are hot-reloaded.
-/// }
-/// </code>
-/// </example>
 [Experimental("ASPIREWATCH001", UrlFormat = "https://aka.ms/aspire/diagnostics/{0}")]
 [AspireDto]
 public sealed class RunConfiguration
@@ -41,11 +28,11 @@ public sealed class RunConfiguration
     internal static RunConfiguration Default { get; } = new();
 
     /// <summary>
-    /// Indicates that the AppHost was started in watch mode.
+    /// Indicates that resources should start in watch mode if able.
     /// </summary>
     /// <remarks>
     /// Integrations that support watch can launch their resources so that source changes are hot-reloaded.
-    /// This is a hint: integrations that cannot watch their resources are free to ignore it.
+    /// This is a hint: integrations that cannot watch their resources should start them in normal fashion.
     /// </remarks>
     public bool WatchEnabled { get; init; }
 }
