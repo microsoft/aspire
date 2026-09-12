df3c6b351f0e9b1c78e9a09a6a3c252ea5f68fd7
Victor Santelé <victor.santele@gmail.com>
2026-09-12 22:51:37 +0200

    fix apphostId

--- src/Aspire.Hosting.JavaScript/JavaScriptHostingExtensions.cs
+++ src/Aspire.Hosting.JavaScript/JavaScriptHostingExtensions.cs
@@ -2217,13 +2217,12 @@ public static partial class JavaScriptHostingExtensions
         appDirectory = PathNormalizer.NormalizePathForCurrentPlatform(Path.Combine(builder.AppHostDirectory, appDirectory));
         var resource = new ViteAppResource(name, "npm", appDirectory);
 
-        return builder.CreateViteAppBuilder(resource, appHostId, appDirectory, runScriptName);
+        return builder.CreateViteAppBuilder(resource, appDirectory, runScriptName);
     }
 
     private static IResourceBuilder<ViteAppResource> CreateViteAppBuilder(
         this IDistributedApplicationBuilder builder,
         ViteAppResource resource,
-        string appHostId,
         string appDirectory,
         string runScriptName)
     {
@@ -3726,10 +3725,8 @@ public static partial class JavaScriptHostingExtensions
         ConfigureWorkspaceContext(resource, workspace, workspaceProjectName);
         ConfigureWorkspaceAppPath(resource, appDirectory, resolvedPackagePath);
 
-        var appHostId = builder.ApplicationBuilder.Configuration["AppHost:Sha256"]![..10].ToLowerInvariant();
-
         var resourceBuilder = builder.ApplicationBuilder
-            .CreateViteAppBuilder(resource, appHostId, appDirectory, runScriptName)
+            .CreateViteAppBuilder(resource, appDirectory, runScriptName)
             .WithParentRelationship(workspace);
 
         WireUpWorkspaceInstaller(builder, resourceBuilder);
